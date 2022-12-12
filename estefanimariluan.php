6007a81ed0378e0063b83ff8aca46544cae0db46
estefani200 <120413743+estefani200@users.noreply.github.com>
2022-12-12 17:23:40 -0300

    Add files via upload

---
+++ estefanimariluan.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+date_default_timezone_set("America/Santiago") . "<br>"; 
+echo "La hora de hoy es : " . date("h:i:s a") . "<br>";
+echo "La fecha de hoy es : " . date("d-m-Y") . "<br>";
+echo "Nombre del estudiante es : Estefani Mariluán". "<br>";
+echo "La asignatura es : Administración De Servicio Web";
+?>
+
+</body>
+</html>
+							
