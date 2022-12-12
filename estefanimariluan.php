32d7621404ee1acad0cb5dce407a1e34cb1fd1e9
yenni254 <120413776+yenni254@users.noreply.github.com>
2022-12-12 17:27:36 -0300

    Update estefanimariluan.php

--- estefanimariluan.php
+++ estefanimariluan.php
@@ -6,7 +6,7 @@
 date_default_timezone_set("America/Santiago") . "<br>"; 
 echo "La hora de hoy es : " . date("h:i:s a") . "<br>";
 echo "La fecha de hoy es : " . date("d-m-Y") . "<br>";
-echo "Nombre del estudiante es : Estefani Mariluán and Joaquin Rainao". "<br>";
+echo "Nombre del estudiante es : Estefani Mariluán, jennifer paine and Joaquin Rainao". "<br>"
 echo "La asignatura es : Administración De Servicio Web";
 ?>
 
